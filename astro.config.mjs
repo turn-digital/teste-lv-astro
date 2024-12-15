6dd854f39a0af2fd71f3f60dd118644d093fc536
DoofisS <thevlad2698@gmail.com>
2024-12-15 20:11:51 +0200

    add new page and markdown plugin sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({ site: "https://www.teste.lv/" });
+export default defineConfig({
+  site: "https://www.teste.lv/",
+  integrations: [sitemap()],
+});
